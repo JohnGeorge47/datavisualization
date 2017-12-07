3d853462020e6a60cd8d66d84fb9808d5e83c924
John <john.george@mountblue.io>
2017-12-07 16:36:45 +0530

    Now we do it

--- index.js
+++ index.js
@@ -16,24 +16,76 @@ fs.createReadStream('primaryschool.csv')
   .on('end', function () {
     // We are done
 
-    var g= obj.district_name.reduce(function(total,district){
+    var districts= obj.district_name.reduce(function(total,district){
        total[district] = (total[district] || 0) + 1 ;
         return total;
 
     },{});
-     var i= obj.block_name.reduce(function(total,block){
+     var block= obj.block_name.reduce(function(total,block){
        total[block] = (total[block] || 0) + 1 ;
         return total;
 
     },{});
-      var n= obj.moi.reduce(function(total,mo){
+      var moi= obj.moi.reduce(function(total,mo){
        total[mo] = (total[mo] || 0) + 1 ;
         return total;
 
     },{});
 
-    console.log(i);
-})
+      // var chart={
+     var chart=   {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Number of schools per district in Karnataka'
+    },
+    subtitle: {
+        text: 'Source: Some org'
+    },
+    xAxis: {
+        categories: [],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Rainfall (mm)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Karnataka',
+        data: []
+    }]
+};
+
+      // var n=Object.keys(districts);
+      // console.log(n);
+      for(var i in districts)
+      {
+        chart.xAxis.categories.push(i);
+        chart.series[0].data.push(districts[i]);
+
+      }
+      var json=JSON.stringify(chart);
+      var fs = require('fs');
+      fs.writeFile('myjsonfile.json', json, 'utf8');
+
+});
 
 
 // var dat;
