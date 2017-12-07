7341b2a958095355aab81194b6ff703106a3879c
John <john.george@mountblue.io>
2017-12-07 11:11:34 +0530

    base added

---
+++ index.js
@@ -0,0 +1,17 @@
+fs = require('fs');
+var dat;
+var districts = [];
+var blocks = [];
+var moi = [];
+var districtnames = [];
+
+fs.readFile('primaryschool.csv', 'utf8', function(err, data) {
+
+if (err) {
+  return console.log(err);
+}
+
+
+});
+
+
