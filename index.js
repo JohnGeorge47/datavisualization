4fa25ce0b4dc3be72db10671d7a4e46b20cbf747
John <john.george@mountblue.io>
2017-12-07 14:37:46 +0530

    Use the npm csv parser library

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-fs = require('fs');
-var dat;
-var districts = [];
-var blocks = [];
-var moi = [];
-var districtnames = [];
+var csv = require('csv-parser');
+var fs= require('fs');
+var obj={
+    "district_name":[],
+    "moi":[],
+    "block_name":[]
+};
+let res=[]
+fs.createReadStream('primaryschool.csv')
+  .pipe(csv())
+  .on('data', function (data) {
+    obj.district_name.push(data.district_name);
+    obj.moi.push(data.moi);
+    obj.block_name.push(data.block_name);
+  })
+  .on('end', function () {
+    // We are done
+    console.log(obj);
+})
 
-fs.readFile('primaryschool.csv', 'utf8', function(err, data) {
 
-if (err) {
-  return console.log(err);
-}
+// var dat;
+// var districts = [];
+// var blocks = [];
+// var moi = [];
+// var districtnames = [];
 
+// fs.readFile('primaryschool.csv', 'utf8', function(err, data) {
 
-});
+// if (err) {
+//   return console.log(err);
+// }
+// var parse = require('csv-parse');
+// require('should');
+
+// var output = [];
+
+// });
 
 
