9ae9a75b5dfc7a85c7b8adc01576db26e5a12b2c
John <john.george@mountblue.io>
2017-12-08 19:32:17 +0530

    Public folder containing files

---
+++ public/app.js
@@ -0,0 +1,35 @@
+console.log("test")
+$(document).ready(function(){
+
+
+    $.ajax({
+       type: "GET",
+       url :"myjsonfile.json",
+       success:function(data)
+       {
+         console.log(data);
+         Highcharts.chart('container', data);
+
+
+
+       },
+       dataType:"json",
+    });
+    $.ajax({
+       type: "GET",
+       url :"nextjsonfile.json",
+       success:function(data)
+       {
+         console.log(data);
+         Highcharts.chart('container2', data);
+
+
+
+       },
+       dataType:"json",
+    });
+
+
+
+
+});
